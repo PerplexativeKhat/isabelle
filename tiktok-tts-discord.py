f1a8cabcd086d9cb364deb9a3ada9778a9b4e2ed
Max Chunsi <m@xic.email>
2022-08-08 00:34:41 +0100

    📝 Add TODOs

--- tiktok-tts-discord.py
+++ tiktok-tts-discord.py
@@ -10,6 +10,8 @@ bot.load("interactions.ext.files")
 
 ENDPOINT_URL = "https://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/?text_speaker=en_us_001&req_text=%TEXT%&speaker_map_type=0"
 
+# TODO: Add /say command
+
 
 @bot.command(
     name="tts",
@@ -27,16 +29,18 @@ ENDPOINT_URL = "https://api16-normal-useast5.us.tiktokv.com/media/api/text/speec
                     type=interactions.OptionType.BOOLEAN,
                     required=False,
                 ),
+        # TODO: Add other voice options
     ]
 )
 async def tts(ctx: interactions.CommandContext, script: str, hide: bool = False):
     orig = str(script)
     script = script.replace("\n", " ").replace(" ", "+").replace("&", "and")
     # TikTok API found by https://github.com/oscie57/tiktok-voice
+    # TODO: Replace with aiohttp, requests is blocking!
     r = requests.post(ENDPOINT_URL.replace("%TEXT%", script))
     vstr = [r.json()["data"]["v_str"]][0]
     b64d = base64.b64decode(vstr)
     file = interactions.File(filename="script.mp3", fp=b64d)
-    await ctx.send(f"> {'||' if hide else ''}{orig}{'||' if hide else ''}", files=file)
+    await ctx.send(f"> {f'||' if hide else ''}{orig}{'||' if hide else ''}", files=file)
 
 bot.start()
