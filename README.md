a6903623854299492b3cb76cbf4dc047229f867b
Max Chunsi <m@xic.email>
2022-08-20 12:56:32 +0100

    Added /say command and updated to use pycord

--- README.md
+++ README.md
@@ -6,22 +6,25 @@ Leverages the TikTok TTS API to send voice messages to Discord.
 
 [oscie57/tiktok-voice](https://github.com/oscie57/tiktok-voice) for providing the TikTok text-to-speech API in a Python module
 
-[interactions-py](https://github.com/interactions-py) for the endlessly useful `interactions` module
+[interactions-py](https://github.com/interactions-py) and [pycord](https://dev.pycord.dev) for the endlessly useful `interactions` and `pycord` modules respectively
+
+[Armster15](https://github.com/Rapptz/discord.py/issues/5192#issuecomment-669515571) for the fixed version of FFmpegPCMAudio.py
 
 ## Running
 
 Create a `.env` file with the following ⬇️
 
 ````sh
-TIKTOK-TTS-DISCORD-TOKEN=<your Discord token>```
-````
+TIKTOK-TTS-DISCORD-TOKEN=<your Discord token>
+```
 
 Then just run `python3 tiktok-tts-discord.py` 🚀
 
 ## Commands
 
-- `/tts script:<script> hide:<True/False>`  
+- `/tts script:<script> hide:<True/False>`
   Post a message to the channel with the chosen script. You can spoiler the text message by setting `hide:True`.
 
-- **Coming soon...** ~~`/say script:<script>`~~  
+- `/say script:<script>`
   Join the current voice channel and say the script out loud.
+````
--- tiktok-tts-discord.py
+++ tiktok-tts-discord.py
@@ -1,46 +1,61 @@
-import interactions
 import dotenv
 from os import getenv
 import requests
 import base64
+# Uses pycord, not discord.py (docs.pycord.dev)
+import discord
+from io import BytesIO
+import time
+from FFmpegPCMAudio import FFmpegPCMAudio
 
 dotenv.load_dotenv()
-bot = interactions.Client(token=getenv("TIKTOK-TTS-DISCORD-TOKEN"))
-bot.load("interactions.ext.files")
+bot = discord.Bot()
 
 ENDPOINT_URL = "https://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/?text_speaker=en_us_001&req_text=%TEXT%&speaker_map_type=0"
 
-# TODO: Add /say command
+# TODO: DRY
 
 
-@bot.command(
+@bot.slash_command(
+    name="say",
+    description="Get the TikTok text-to-speech lady to say something in a voice chat",)
+async def say(ctx, script: discord.Option(str, "What would you like her to say?")):
+    if not ctx.author.voice:
+        await ctx.respond("Join a voice channel first!", ephemeral=True)
+    else:
+        if len(script) > 200:
+            await ctx.respond(f"Shorten your script please! The API struggles if it's longer than 200 characters (yours was {len(script)})")
+        else:
+            script = script.replace("\n", " ").replace(
+                " ", "+").replace("&", "and")
+            # TikTok API found by https://github.com/oscie57/tiktok-voice
+            # TODO: Replace with aiohttp, requests is blocking!
+            r = requests.post(ENDPOINT_URL.replace("%TEXT%", script))
+            vstr = [r.json()["data"]["v_str"]][0]
+            b64d = base64.b64decode(vstr)
+            await ctx.respond(f"I'm joining <#{ctx.author.voice.channel.id}> now...", ephemeral=True)
+            vc = await ctx.author.voice.channel.connect()
+            vc.play(FFmpegPCMAudio(BytesIO(b64d).getvalue(), pipe=True))
+            while vc.is_playing():
+                time.sleep(1)
+            await vc.disconnect()
+
+
+@bot.slash_command(
     name="tts",
-    description="Get the TikTok text-to-speech lady to say something.",
-    options=[
-                interactions.Option(
-                    name="script",
-                    description="What do you want her to say?",
-                    type=interactions.OptionType.STRING,
-                    required=True,
-                ),
-        interactions.Option(
-                    name="hide",
-                    description="Do you want to spoiler the text?",
-                    type=interactions.OptionType.BOOLEAN,
-                    required=False,
-                ),
-        # TODO: Add other voice options
-    ]
-)
-async def tts(ctx: interactions.CommandContext, script: str, hide: bool = False):
+    description="Get the TikTok text-to-speech lady to say something as an mp3 file",)
+# TODO: Add other voice options
+async def tts(ctx, script: discord.Option(str, "What would you like her to say?"), hide: discord.Option(bool, "Hide your message?") = False):
     orig = str(script)
     script = script.replace("\n", " ").replace(" ", "+").replace("&", "and")
-    # TikTok API found by https://github.com/oscie57/tiktok-voice
-    # TODO: Replace with aiohttp, requests is blocking!
-    r = requests.post(ENDPOINT_URL.replace("%TEXT%", script))
-    vstr = [r.json()["data"]["v_str"]][0]
-    b64d = base64.b64decode(vstr)
-    file = interactions.File(filename="script.mp3", fp=b64d)
-    await ctx.send(f"> {f'||' if hide else ''}{orig}{'||' if hide else ''}", files=file)
+    if len(script) > 200:
+        await ctx.respond(f"Shorten your script please! The API struggles if it's longer than 200 characters (yours was {len(script)})")
+    else:
+        # TikTok API found by https://github.com/oscie57/tiktok-voice
+        # TODO: Replace with aiohttp, requests is blocking!
+        r = requests.post(ENDPOINT_URL.replace("%TEXT%", script))
+        vstr = [r.json()["data"]["v_str"]][0]
+        b64d = base64.b64decode(vstr)
+        await ctx.respond(f"> {f'||' if hide else ''}{orig}{'||' if hide else ''}", file=discord.File(BytesIO(b64d), filename="script.mp3"))
 
-bot.start()
+bot.run(getenv("TIKTOK-TTS-DISCORD-TOKEN"))
